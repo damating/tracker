45635b34bae832b2e222477bf56c66572e0a2844
damating <paula.grzesiak@gmail.com>
2015-04-24 13:45:35 +0000

    Basic home/about tests

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionController::TestCase
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About"
+  end
+end
